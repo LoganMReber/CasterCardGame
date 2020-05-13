cfb7fa4f86579ad438b27dae1b452f6418dc03ba
LoganMReber <loganmreber@gmail.com>
2020-05-13 17:53:02 -0500

    test

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React, { useState } from 'react';
 import Card from './components/Card';
-import ManaPool from './components/ManaPool';
+// import ManaPool from './components/ManaPool';
 import ManaGain from './components/ManaGain';
-import {cards} from './cardData';
+import { cards } from './cardData';
+import Game from './components/Game';
 function App() {
-  const [mana,setMana] = useState({red:0,green:0,blue:0,orange:0});
-  const [showMG,setShowMG] = useState(true)
+  const [mana, setMana] = useState({ red: 0, green: 0, blue: 0, orange: 0 });
+  const [showMG, setShowMG] = useState(true);
   return (
-    <div className="App">
-      {showMG ? <ManaGain mana={mana} setMana={setMana} setShowMG={setShowMG}/>:null}
-      <div className='Cards'>
-        {cards.map((card,id)=><Card key={id} data={card}/>)}
-      </div>
-      <ManaPool mana={mana}/>
+    <div className='App'>
+      <Game />
+      {/* {showMG ? (
+        <ManaGain mana={mana} setMana={setMana} setShowMG={setShowMG} />
+      ) : null} */}
+      {/* <div className='Cards'>
+        {cards.map((card, id) => (
+          <Card key={id} data={card} />
+        ))}
+      </div> */}
     </div>
   );
 }
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,29 +1,57 @@
-const Game = props => {
-    while(props.player1.health > 0 && props.player2.health > 0){
-        switch(props.phase){
-            case "P1_MANA":
-                return <div>Player 1 Mana Phase</div>
-            case "P1_MAIN":
-                return <div>Player 1 Main Phase</div>
-            case "P1_ATTACK":
-                return <div>Player 1 Attacking Phase</div>
-            case "P1_BLOCK":
-                return <div>Player 1 Blocking Phase</div>
-            case "P1_END":
-                return <div>Player 1 End Phase</div>
-            case "P2_MANA":
-                return <div>Player 2 Mana Phase</div>
-            case "P2_MAIN":
-                return <div>Player 2 Main Phase</div>
-            case "P2_ATTACK":
-                return <div>Player 2 Attacking Phase</div>
-            case "P2_BLOCK":
-                return <div>Player 2 Blocking Phase</div>
-            case "P2_END":
-                return <div>Player 2 End Phase</div>
-            default:
-                window.alert('Lost phase!')
-                return <div>Error</div>
-        }
-    }
-}
+import React from 'react';
+import ManaPool from './ManaPool';
+const Game = (props) => {
+  const p1Mana = { red: 1, green: 0, blue: 2, orange: 0 };
+  const p2Mana = { red: 0, green: 2, blue: 0, orange: 1 };
+  return (
+    <div className='Game'>
+      <div className='play_area'>
+        <div className='far_board'>
+          <div className='creatures'>Creature Zone</div>
+          <div className='info'>
+            <ManaPool mana={p2Mana} />
+            <div className='health'>♥ 20</div>
+          </div>
+        </div>
+        <div className='effects_stack'>Active Effects Zone</div>
+        <div className='close_board'>
+          <div className='info'>
+            <div className='health'>20 ♥</div>
+            <ManaPool mana={p1Mana} />
+          </div>
+          <div className='creatures'>Creature Zone</div>
+        </div>
+      </div>
+      <div className='cards_menu'>
+        <div className='red'>
+          <img src='./img/beast.png' alt='creature' />
+        </div>
+        <div className='green'>
+          <img src='./img/beast.png' alt='creature' />
+        </div>
+        <div className='blue'>
+          <img src='./img/beast.png' alt='creature' />
+        </div>
+        <div className='orange'>
+          <img src='./img/beast.png' alt='creature' />
+        </div>
+      </div>
+      <div className='cards_menu'>
+        <div className='red'>
+          <img src='./img/spell.png' alt='spell' />
+        </div>
+        <div className='green'>
+          <img src='./img/spell.png' alt='spell' />
+        </div>
+        <div className='blue'>
+          <img src='./img/spell.png' alt='spell' />
+        </div>
+        <div className='orange'>
+          <img src='./img/spell.png' alt='spell' />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Game;
